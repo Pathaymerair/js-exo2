9a756dfbfea90b6079fb1cb2acae6e36d84d4fad
Pathaymerair <charlomartineau@gmail.com>
2018-07-02 15:57:24 +0200

    phase 7

---
+++ public/js/main.js
@@ -0,0 +1,24 @@
+var guerrier = {
+  nom : "Kevin le Yoloswag",
+  age : 12,
+  taille : "20 cm",
+  force : 3,
+  arme : "Une spatule"
+};
+
+var mage = {
+  nom : "Sananas",
+  age : "25",
+  taille : "2 mètres ciinquante",
+  puissance : "Plus de 9000",
+  arme : "Une poêle de feu",
+};
+
+var paysan = {
+  nom : "John Snow",
+  age : "2 et demi",
+  taille : "osef en vrai",
+  force : "-9000",
+  arme : "Le pouvoir de l'inutilité",
+
+};
